b49073b6ef329eb95a14b7e4a85425a3849d418d
Oleksii Nazarenko <alexeybread81@gmail.com>
2022-07-17 22:13:38 +0100

    small fixes

--- components/UI/shop/ProductCard/ProductCard.js
+++ components/UI/shop/ProductCard/ProductCard.js
@@ -5,21 +5,21 @@ import { motion } from 'framer-motion'
 import { cardTranstion } from '@/lib/animations'
 
 export default function ProductCard({ data, onClick }) {
-    const { name, price, img, created } = data.product
-    // authorUid: "F6Zy5tqIQcNL0Ggl34cll48Jcb53"
-    // authorUsername: "asd"
-    // product: { price: 2, name: '12', img: '3', created: 1657295564773, sold: 0, … }
-    return <motion.div className={s.container} onClick={onClick} layoutId={name + img}  >
-        <motion.div className={cn([s.name, s.item])} layoutId={name + created}  >
-            {name}
-        </motion.div>
+    const { name, price, img } = data.product
+
+    return <div className={s.container} onClick={onClick}>
+        <div className={cn([s.name, s.item])}>
+            <div className={s.name}>
+                {name}
+            </div>
+        </div>
 
         <div layout className={cn([s.imgContainer, s.item])}>
             <img src={img} className={s.img} />
         </div>
 
-        <motion.div layoutId={name + price} className={cn([s.price, s.item])}  >
+        <div className={cn([s.price, s.item])}  >
             <pre>{price}<Mc> Mc</Mc></pre>
-        </motion.div>
-    </motion.div>
+        </div>
+    </div>
 }

5df581949d3f8e76c324e44f7853ba4adc6be002
Oleksii Nazarenko <alexeybread81@gmail.com>
2022-07-18 12:07:10 +0100

    fix overflows

--- components/UI/Header/Header.js
+++ components/UI/Header/Header.js
@@ -4,7 +4,9 @@ import s from './Header.module.css'
 export default function Header({ subheader, cta, children }) {
     return <div className={s.container}>
         <div>
-            <h3 className={s.header}>{children}</h3>
+            <h3 className={s.header}>
+                {children}
+            </h3>
             <h5 className={s.subheader}>{subheader}</h5>
         </div>
 
